cf79d92d679932269ee4cf909b92235c63cace32
irrationnelle <drakkarverenis@gmail.com>
2020-08-14 22:57:41 +0900

    feat: add first example for tutorial

---
+++ src/components/Task.stories.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { action } from "@storybook/addon-actions";
+
+import Task from "./Task";
+
+export default {
+    component: "Task",
+    title: "Task",
+    excludeStories: /.*Data$/
+};
+
+export const taskData = {
+    id: "1",
+    title: "Test Task",
+    state: "TASK_INBOX",
+    updateAt: new Date()
+};
+
+export const actionData = {
+    onPinTask: action("onPinTask"),
+    onArchiveTask: action("onArchiveTask")
+};
+
+export const Default = () => <Task task={{ ...taskData }} {...actionData} />;
+
+export const Pinned = () => (
+    <Task task={{ ...taskData, state: "TASK_PINNED" }} {...actionData} />
+);
+
+export const Archived = () => (
+    <Task task={{ ...taskData, state: "TASK_ARCHIVED" }} {...actionData} />
+);
---
+++ src/components/Task.tsx
@@ -0,0 +1,51 @@
+import React, { ReactElement } from "react";
+
+interface Props {
+    task: {
+        id: string;
+        title: string;
+        state: string;
+    };
+    onArchiveTask: (id: string) => void;
+    onPinTask: (id: string) => void;
+}
+
+const Task: React.FC<Props> = ({
+    task: { id, title, state },
+    onArchiveTask,
+    onPinTask
+}: Props): ReactElement => (
+    <div className={`list-item ${state}`}>
+        <label className="checkbox">
+            <input
+                type="checkbox"
+                defaultChecked={state === "TASK_ARCHIVED"}
+                disabled={true}
+                name="checked"
+            />
+            <span
+                className="checkbox-custom"
+                onClick={() => onArchiveTask(id)}
+            />
+        </label>
+        <div className="title">
+            <input
+                type="text"
+                value={title}
+                readOnly={true}
+                placeholder="Input title"
+            />
+        </div>
+
+        <div className="actions" onClick={event => event.stopPropagation()}>
+            {state !== "TASK_ARCHIVED" && (
+                // eslint-disable-next-line jsx-a11y/anchor-is-valid
+                <a onClick={() => onPinTask(id)}>
+                    <span className={`icon-star`} />
+                </a>
+            )}
+        </div>
+    </div>
+);
+
+export default Task;
